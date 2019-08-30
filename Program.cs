3b5b98205854f4ebcf26b37d2bc6f0269c5433fa
Ricardo Felix <ricardofelix2@yahoo.com>
2019-08-30 13:27:47 -0400

    Added Analytics, lol

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace WindowsFormsApp1
 {
@@ -14,6 +17,9 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+	        AppCenter.Start("53e28105-836a-4c7c-9e58-3d720a6fb5c2",
+		        typeof(Analytics), typeof(Crashes));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SubDirectoryForm());
